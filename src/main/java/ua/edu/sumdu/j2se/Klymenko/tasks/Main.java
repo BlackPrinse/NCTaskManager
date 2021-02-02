c455c41488b8a0ed576b6f1c59bb32e77b9fa2d6
Ralph <bogdanfirefly@gmail.com>
2021-02-02 05:22:14 +0200

    finaly

---
+++ src/main/java/ua/edu/sumdu/j2se/Klymenko/tasks/Main.java
@@ -0,0 +1,11 @@
+package ua.edu.sumdu.j2se.Klymenko.tasks;
+
+public class Main {
+    /**
+     * The main method. This is where the execution of the program begins.
+     * */
+    public static void main(String[] args) throws ClassNotFoundException {
+        TaskManager manager = new TaskManager();
+        manager.startManager();
+    }
+}
